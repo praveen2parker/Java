ce2b5e6a400220be58bfef53a99bb1f754ede541
praveen2parker <87643056+praveen2parker@users.noreply.github.com>
2021-08-23 12:41:02 +0530

    Update Pattern Printing1.java

--- Pattern Printing1.java
+++ Pattern Printing1.java
@@ -9,9 +9,9 @@ public class PatterPrinting{
             for(int j=2*(row-i);j>=0;j--){
                 System.out.print(" ");
             }
-            for(int j=0;j<=i;j++){
-                System.out.print("* ");
-            }
+                for(int j=0;j<=i;j++){
+                    System.out.print("* ");
+                 }
             System.out.println();
         }
     }
