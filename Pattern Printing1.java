9258755c6b0e30a744c6bb4997df90d09342e8e1
praveen2parker <87643056+praveen2parker@users.noreply.github.com>
2021-08-23 12:36:57 +0530

    Create Pattern Printing1.java

---
+++ Pattern Printing1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class PatterPrinting{
+    public static void main(String[] args) {
+        int row;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of Row");
+        row=sc.nextInt();
+        for(int i=0;i<=row-1;i++){
+            for(int j=2*(row-i);j>=0;j--){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
