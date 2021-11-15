3e13d6a138056d02ab763df0b9afa533feb49727
praveen2parker <87643056+praveen2parker@users.noreply.github.com>
2021-11-15 17:44:11 +0530

    Create String1.java

---
+++ String1.java
@@ -0,0 +1,24 @@
+//Finding non repeating second given string 
+public class MyClass {
+    public static void main(String args[]) {
+      String s= "abaacbadaeefgg";
+      int count,temp=0;
+      for(int i=0;i<s.length();i++){
+          count =0;
+          for(int j=0;j<s.length();j++){
+              if(s.charAt(i)==s.charAt(j)){
+                  count++;
+              }
+          }
+          if(count==1){
+              temp++;
+              if(temp==2){
+               System.out.print(s.charAt(i));   
+              }
+              else{
+                  System.out.print("not a repeating ");
+              }
+          }
+      }
+    }
+}
